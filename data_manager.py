59698b8a9805c57c5242540acfc1813814f42294
Rahul Kanna.A.V <91176771+Nickobellic@users.noreply.github.com>
2022-09-15 19:31:03 +0530

    Used this Python Code to Preprocess the Google Sheet

---
+++ data_manager.py
@@ -0,0 +1,10 @@
+
+from flight_search import FlightSearch
+
+class DataManager:
+    #This class is responsible for talking to the Google Sheet.
+    def __init__(self):
+        self.fill()
+
+    # def fill(self):
+    #
