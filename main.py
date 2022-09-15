b200111e3a3709544e64d93c3ca1ded0a1ef3a3b
Rahul Kanna.A.V <91176771+Nickobellic@users.noreply.github.com>
2022-09-15 19:33:45 +0530

    Main Python file which connects all these modules

---
+++ main.py
@@ -0,0 +1,5 @@
+#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from notification_manager import NotificationManager
+
+send = NotificationManager()
+send.create_emails()
